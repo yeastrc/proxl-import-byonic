33853449bb7d41c5b24bf660469b4fa3ef02456f
Michael Riffle <mriffle@uw.edu>
2022-11-04 13:36:23 -0700

    Update version number.

--- src/main/java/org/yeastrc/proxl/xml/byonic/constants/Constants.java
+++ src/main/java/org/yeastrc/proxl/xml/byonic/constants/Constants.java
@@ -2,6 +2,6 @@ package org.yeastrc.proxl.xml.byonic.constants;
 
 public class Constants {
     public static final String CONVERSION_PROGRAM_URI = "https://github.com/yeastrc/proxl-import-byonic";
-    public static final String CONVERSION_PROGRAM_VERSION = "1.2.0";
+    public static final String CONVERSION_PROGRAM_VERSION = "1.2.1";
     public static final String CONVERSION_PROGRAM_NAME = "byonic2ProxlXML.jar";
 }
